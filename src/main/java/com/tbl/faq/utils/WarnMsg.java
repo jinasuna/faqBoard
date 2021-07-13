0c520bb20386c7ee356e547dec9c2a0680ddfd3f
jinasuna <jinasuna@github.com>
2021-07-14 01:50:58 +0900

    jpa 없애기 위해 패키지 통째로 삭

--- src/main/java/com/tbl/faq/utils/WarnMsg.java
+++
@@ -1,6 +0,0 @@
-package com.tbl.faq.utils;
-
-public class WarnMsg {
-    public static final String NO_RESULT_BOARD_MSG = "해당 시퀀스의 게시물이 존재하지 않습니다.";
-    public static final String KST = "Asia/Seoul";
-}
